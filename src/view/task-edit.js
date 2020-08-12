d36e96e2cab4deb7e6196b57dd200c971bfe613c
vzuev-ha <65515070+vzuev-ha@users.noreply.github.com>
2020-08-13 01:20:20 +0300

    3.11 Обучаем шаблон формы принимать данные. WIP

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -1,6 +1,20 @@
-export const createTaskEditTemplate = () => {
-  return (
-    `<article class="card card--edit card--yellow card--repeat">
+export const createTaskEditTemplate = (task = {}) => {
+  const {
+    color = `black`,
+    description = ``,
+    dueDate = null,
+    repeating = {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false
+    }
+  } = task;
+
+  return `<article class="card card--edit card--${color} card--repeat">
       <form class="card__form" method="get">
         <div class="card__inner">
           <div class="card__color-bar">
@@ -8,13 +22,14 @@ export const createTaskEditTemplate = () => {
               <use xlink:href="#wave"></use>
             </svg>
           </div>
+
           <div class="card__textarea-wrap">
             <label>
               <textarea
                 class="card__text"
                 placeholder="Start typing your text here..."
                 name="text"
-              >This is example of task edit. You can set date and chose repeating days and color.</textarea>
+              >${description}</textarea>
             </label>
           </div>
           <div class="card__settings">
@@ -189,6 +204,5 @@ export const createTaskEditTemplate = () => {
           </div>
         </div>
       </form>
-    </article>`
-  );
+    </article>`;
 };

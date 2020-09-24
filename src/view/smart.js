45d66f1d98f9f22442b6896e508cdf9fd6f37957
vzuev-ha <65515070+vzuev-ha@users.noreply.github.com>
2020-09-25 01:06:51 +0300

    6.1.17 Ограничиваем количество форм за раз

--- src/view/smart.js
+++ src/view/smart.js
@@ -12,9 +12,9 @@ export default class Smart extends Abstract {
     }
 
     this._data = Object.assign(
-      {},
-      this._data,
-      update
+        {},
+        this._data,
+        update
     );
 
     if (justDataUpdating) {

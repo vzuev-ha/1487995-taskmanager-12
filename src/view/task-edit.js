7f2edaa7b4a25c38a71dc6a5427ab0c82372962b
vzuev-ha <65515070+vzuev-ha@users.noreply.github.com>
2020-09-25 00:17:17 +0300

    Исправляем ошибку автора с датой (при откате примера 'в лоб' забыли о тех, кто в лоб не делал), затем поправляем отступы, чтобы соответствовать файлу в уроке.

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -31,7 +31,7 @@ const createTaskEditDateTemplate = (dueDate, isDueDate) => {
           type="text"
           placeholder=""
           name="date"
-          value="${humanizeTaskDueDate(dueDate)}"
+          value="${dueDate !== null ? humanizeTaskDueDate(dueDate) : ``}"
         />
       </label>
     </fieldset>` : ``}
@@ -92,48 +92,48 @@ const createTaskEditTemplate = (data) => {
   const colorsTemplate = createTaskEditColorsTemplate(color);
 
   return `<article class="card card--edit card--${color} ${deadlineClassName} ${repeatingClassName}">
-      <form class="card__form" method="get">
-        <div class="card__inner">
-          <div class="card__color-bar">
-            <svg class="card__color-bar-wave" width="100%" height="10">
-              <use xlink:href="#wave"></use>
-            </svg>
-          </div>
+    <form class="card__form" method="get">
+      <div class="card__inner">
+        <div class="card__color-bar">
+          <svg class="card__color-bar-wave" width="100%" height="10">
+            <use xlink:href="#wave"></use>
+          </svg>
+        </div>
 
-          <div class="card__textarea-wrap">
-            <label>
-              <textarea
-                class="card__text"
-                placeholder="Start typing your text here..."
-                name="text"
-              >${description}</textarea>
-            </label>
-          </div>
+        <div class="card__textarea-wrap">
+          <label>
+            <textarea
+              class="card__text"
+              placeholder="Start typing your text here..."
+              name="text"
+            >${description}</textarea>
+          </label>
+        </div>
 
-          <div class="card__settings">
-            <div class="card__details">
-              <div class="card__dates">
-                ${dateTemplate}
+        <div class="card__settings">
+          <div class="card__details">
+            <div class="card__dates">
+              ${dateTemplate}
 
-                ${repeatingTemplate}
-              </div>
-            </div>
-
-            <div class="card__colors-inner">
-              <h3 class="card__colors-title">Color</h3>
-              <div class="card__colors-wrap">
-                ${colorsTemplate}
-              </div>
+              ${repeatingTemplate}
             </div>
           </div>
 
-          <div class="card__status-btns">
-            <button class="card__save" type="submit">save</button>
-            <button class="card__delete" type="button">delete</button>
+          <div class="card__colors-inner">
+            <h3 class="card__colors-title">Color</h3>
+            <div class="card__colors-wrap">
+              ${colorsTemplate}
+            </div>
           </div>
         </div>
-      </form>
-    </article>`;
+
+        <div class="card__status-btns">
+          <button class="card__save" type="submit">save</button>
+          <button class="card__delete" type="button">delete</button>
+        </div>
+      </div>
+    </form>
+  </article>`;
 };
 
 export default class TaskEdit extends AbstractView {
